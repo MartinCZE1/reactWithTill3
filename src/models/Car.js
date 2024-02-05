86c6fe3c68069f9f25013382a651fb234d28991d
Martin Hercík <martinhercik16@gmail.com>
2024-02-05 15:30:22 +0100

    Huge change

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,4 +1,56 @@
-export const getAllCars = () => {};
-export const getCar = () => {};
-export const createCar = () => {};
-export const deleteCar = () => {};
+export const getAllCars = async () => {
+  const req = await fetch("http://localhost:3000/cars", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "GET",
+  });
+  const data = await req.json();
+};
+
+export const getCar = async (id) => {
+  const req = await fetch(`http://localhost:3000/cars/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "GET",
+  });
+  const data = await req.json();
+};
+
+export const createCar = async (formData) => {
+  const req = await fetch("http://localhost:3000/cars", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+  const data = await req.json();
+};
+
+export const updateCar = async (id, formData) => {
+  const req = await fetch(`http://localhost:3000/cars/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "PUT",
+    body: JSON.stringify(formData),
+  });
+  const data = await req.json();
+};
+
+export const deleteCar = async (id) => {
+  const req = await fetch(`http://localhost:3000/cars/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "DELETE",
+  });
+  const data = await req.json();
+};
