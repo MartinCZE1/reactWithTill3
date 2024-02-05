ebabe66755b1091d8de8a42eafe62759e03721d6
Martin Hercík <martinhercik16@gmail.com>
2024-02-05 15:30:55 +0100

    Huge format

--- src/pages/CarCreateForm/CarCreateForm.jsx
+++ src/pages/CarCreateForm/CarCreateForm.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
-import MainPage from "../MainPage/MainPage"
+import { Link } from "react-router-dom";
+import MainPage from "../MainPage/MainPage";
 
 export default function CarCreateForm() {
-    return(
-        <>
-            <h1>Car create form</h1>
-            <Link to={"/"}>
-            <p>Go back</p>
-            </Link>
-        </>
-    )
+  return (
+    <>
+      <h1>Car create form</h1>
+      <Link to={"/"}>
+        <p>Go back</p>
+      </Link>
+    </>
+  );
 }
